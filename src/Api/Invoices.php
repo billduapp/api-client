eda2d1e0db74032696db00a7fe99a91ecf7ed937
Martin Bažík <martin@bazo.sk>
2016-05-23 13:08:03 +0200

    add pay method to invoices

--- src/Api/Invoices.php
+++ src/Api/Invoices.php
@@ -11,4 +11,21 @@ class Invoices extends TypedDocuments
 
 	const TYPE = 'invoice';
 
+	public function pay($id, $amount, $currency = 'EUR', \DateTime $date = NULL)
+	{
+		if (is_null($date)) {
+			$date = new \DateTime;
+		}
+
+		$data = [
+			'amount' => $amount,
+			'currency' => $currency,
+			'createdAt' => $date->format('c')
+		];
+
+
+		return $this->curl->post('/documents/' . $id . '/payments', $data);
+	}
+
+
 }
