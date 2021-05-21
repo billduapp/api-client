034dbda8c388e829a2ffb7713fd2c1abadbe2e89
Pavel Kaspar <xpalko@seznam.cz>
2021-05-21 12:33:06 +0200

    Update Clients.php

--- src/Api/Clients.php
+++ src/Api/Clients.php
@@ -23,9 +23,12 @@ class Clients
 	/**
 	 * @return \iInvoices\Api\Responses\Collection
 	 */
-	public function listAll()
+	public function listAll($page = 1, $limit = 10)
 	{
-		$response = $this->curl->get('/clients');
+		$response = $this->curl->get('/clients', [
+			'page'	 => $page,
+			'limit'	 => $limit,
+		]);
 
 		return $response;
 	}
